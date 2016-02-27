21b79927fe84ec7385e314bbad7969a2324a04dd
Melnyk Vladimir <melnik@dev.expoglobus.com>
2016-02-27 17:51:52 +0200

    init

---
+++ init.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+sudo rm /etc/nginx/sites-enabled/default
+
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo /etc/init.d/nginx restart
+
+#gunicorn -c etc/gunicorn_conf.py hello:application &
